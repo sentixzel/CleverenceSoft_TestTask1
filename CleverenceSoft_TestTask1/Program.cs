f91a44e73a1c7ea0ad1128b165083bc728f23f03
sentixzel <sentixzel@gmail.com>
2025-06-10 16:50:11 +0300

    Добавьте файлы проекта.

---
+++ CleverenceSoft_TestTask1/Program.cs
@@ -0,0 +1,52 @@
+﻿
+namespace CleverenceSoft_TestTask1
+{
+    public class Programm
+    {
+        public static void Main(string[] args)
+        {
+            //Код к первой задаче
+            string originalString = "aaabbcccdde";
+            string compressedString = StringCompression.CompressString(originalString);
+            string decompressedString = StringCompression.DecompressString(compressedString);
+
+            Console.WriteLine($"Исходная строка: {originalString}");
+            Console.WriteLine($"Сжатая строка: {compressedString}");
+            Console.WriteLine($"Декомпрессированная строка: {decompressedString}");
+
+            originalString = "abbb";
+            compressedString = StringCompression.CompressString(originalString);
+            decompressedString = StringCompression.DecompressString(compressedString);
+
+            Console.WriteLine($"\nИсходная строка: {originalString}");
+            Console.WriteLine($"Сжатая строка: {compressedString}");
+            Console.WriteLine($"Декомпрессированная строка: {decompressedString}");
+
+            originalString = "a";
+            compressedString = StringCompression.CompressString(originalString);
+            decompressedString = StringCompression.DecompressString(compressedString);
+
+            Console.WriteLine($"\nИсходная строка: {originalString}");
+            Console.WriteLine($"Сжатая строка: {compressedString}");
+            Console.WriteLine($"Декомпрессированная строка: {decompressedString}");
+
+            originalString = "";
+            compressedString = StringCompression.CompressString(originalString);
+            decompressedString = StringCompression.DecompressString(compressedString);
+
+            Console.WriteLine($"\nИсходная строка: {originalString}");
+            Console.WriteLine($"Сжатая строка: {compressedString}");
+            Console.WriteLine($"Декомпрессированная строка: {decompressedString}");
+
+            Console.WriteLine("\nВведите свою строку:");
+            originalString = Console.ReadLine();
+            compressedString = StringCompression.CompressString(originalString);
+            decompressedString = StringCompression.DecompressString(compressedString);
+            Console.WriteLine($"\nИсходная строка: {originalString}");
+            Console.WriteLine($"Сжатая строка: {compressedString}");
+            Console.WriteLine($"Декомпрессированная строка: {decompressedString}");
+
+
+        }
+    }
+}
---
+++ CleverenceSoft_TestTask1/StringCompression.cs
@@ -0,0 +1,86 @@
+﻿using System.Text;
+
+namespace CleverenceSoft_TestTask1
+{
+    /// <summary>
+    /// К задаче 1
+    /// </summary>
+    public static class StringCompression
+    {
+        /// <summary>
+        /// Функция компрессии строки
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string CompressString(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                //Здесь можно добавить вывод предупреждения, что строка пустая
+                return "";
+            }
+
+            StringBuilder compressed = new StringBuilder();
+            int count = 1;
+            char currentChar = s[0];
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i] == currentChar)
+                {
+                    count++;
+                }
+                else
+                {
+                    compressed.Append(currentChar);
+                    if (count > 1)
+                    {
+                        compressed.Append(count);
+                    }
+                    currentChar = s[i];
+                    count = 1;
+                }
+            }
+
+            compressed.Append(currentChar);
+            if (count > 1)
+            {
+                compressed.Append(count);
+            }
+
+            return compressed.ToString();
+        }
+
+        /// <summary>
+        /// Функция декомпрессии строки
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string DecompressString(string s)
+        {
+            StringBuilder decompressed = new StringBuilder();
+            for (int i = 0; i < s.Length; i++)
+            {
+                char currentChar = s[i];
+                if (i + 1 < s.Length && char.IsDigit(s[i + 1]))
+                {
+                    StringBuilder countStr = new StringBuilder();
+                    int j = i + 1;
+                    while (j < s.Length && char.IsDigit(s[j]))
+                    {
+                        countStr.Append(s[j]);
+                        j++;
+                    }
+                    int count = int.Parse(countStr.ToString());
+                    decompressed.Append(currentChar, count);
+                    i = j - 1;
+                }
+                else
+                {
+                    decompressed.Append(currentChar);
+                }
+            }
+            return decompressed.ToString();
+        }
+    }
+}
